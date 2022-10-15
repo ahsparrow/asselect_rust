00eee9deb19953b9db2d2127e478c858e3e7ec4a
Alan Sparrow <git@freeflight.org.uk>
2022-10-15 21:04:41 +0100

    Basic yew app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use yew::{function_component, html};
+
+#[function_component(App)]
+fn app() -> Html {
+    html!{
+        <div>
+            <p>{"Hello world"}</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
