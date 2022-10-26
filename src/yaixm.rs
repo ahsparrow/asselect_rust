3707a9745e858e1d3bbfbd4f1b89b40964199602
Alan Sparrow <git@freeflight.org.uk>
2022-10-26 08:30:42 +0100

    Filter default LOAs

--- src/yaixm.rs
+++ src/yaixm.rs
@@ -3,6 +3,7 @@ use serde_json::Value as JsonValue;
 pub fn loa_names(yaixm: &JsonValue) -> Vec<String> {
     let loa = yaixm["loa"].as_array().unwrap();
     loa.iter()
+        .filter(|x| !x["default"].as_bool().unwrap_or(false))
         .map(|x| x["name"].as_str().unwrap().to_string())
         .collect::<Vec<String>>()
 }
