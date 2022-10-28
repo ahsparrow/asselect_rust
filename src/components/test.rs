3f495158248ae2f2e06d8b1945f292805b9a5e45
Alan Sparrow <git@freeflight.org.uk>
2022-10-28 10:37:55 +0100

    Convert test component back to functional

--- src/components/test.rs
+++ src/components/test.rs
@@ -1,7 +1,7 @@
 use crate::LoaSetting;
 use std::collections::HashSet;
 use web_sys::HtmlInputElement;
-use yew::{html, Callback, Component, Context, Event, Html, Properties, TargetCast};
+use yew::{function_component, html, Callback, Event, Html, Properties, TargetCast};
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -10,44 +10,35 @@ pub struct Props {
     pub callback: Callback<LoaSetting>,
 }
 
-pub struct Test;
+#[function_component(Test)]
+pub fn test(props: &Props) -> Html {
+    let onchange = props.callback.reform(|e: Event| {
+        let id = e.target_unchecked_into::<HtmlInputElement>().id();
+        let value = e.target_unchecked_into::<HtmlInputElement>().checked();
 
-impl Component for Test {
-    type Message = ();
-    type Properties = Props;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let onchange = ctx.props().callback.reform(move |e: Event| {
-            let id = e.target_unchecked_into::<HtmlInputElement>().id();
-            let value = e.target_unchecked_into::<HtmlInputElement>().checked();
-            LoaSetting {
-                id: id,
-                checked: value,
-            }
-        });
-
-        html! {
-            <div class="columns is-multiline">
-            {
-                ctx.props().loa.iter().map(|name| {
-                    let checked = ctx.props().selected.contains(name);
-                    html!(
-                          <div class="column is-one-third">
-                            <div class="field">
-                            <label class="checkbox">
-                              <input type="checkbox" class="mr-1" {checked} id={name.clone()} onchange={onchange.clone()} />
-                                {name}
-                            </label>
-                            </div>
-                          </div>
-                        )
-                }).collect::<Html>()
-            }
-            </div>
+        LoaSetting {
+            id: id,
+            checked: value,
         }
+    });
+
+    html! {
+        <div class="columns is-multiline">
+        {
+            props.loa.iter().map(|name| {
+                let checked = props.selected.contains(name);
+                html!(
+                      <div class="column is-one-third">
+                        <div class="field">
+                        <label class="checkbox">
+                          <input type="checkbox" class="mr-1" {checked} id={name.clone()} onchange={onchange.clone()} />
+                            {name}
+                        </label>
+                        </div>
+                      </div>
+                    )
+            }).collect::<Html>()
+        }
+        </div>
     }
 }
