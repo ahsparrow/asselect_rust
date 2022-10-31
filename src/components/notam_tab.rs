44d07fbb8c0b891a425eafbb7fb775a6fbd0cf44
Alan Sparrow <git@freeflight.org.uk>
2022-10-31 16:21:07 +0000

    Add NOTAM tab content

--- src/components/notam_tab.rs
+++ src/components/notam_tab.rs
@@ -4,7 +4,33 @@ use yew::{function_component, html};
 pub fn notam_tab() -> Html {
     html! {
         <div>
-          {"Hello notam tab"}
+          <div class="subtitle">
+            {"NOTAM"}
+          </div>
+          <p class="mb-2">{"The PDFs below show a summary of the navigation warning NOTAMs relevant
+          to cross country gliding"}</p>
+          <p class="mb-4">{"The NOTAMs are refreshed during the day at approximately ten minutes
+          past the hour."}</p>
+          <p class="mb-4">
+            <a href="https://navplot.asselect.uk/today_south.pdf" download={""}>
+              {"Download Today (England/Wales) PDF"}
+            </a>
+          </p>
+          <p class="mb-4">
+            <a href="https://navplot.asselect.uk/today_north.pdf" download={""}>
+              {"Download Today (North England/Scotland) PDF"}
+            </a>
+          </p>
+          <p class="mb-4">
+            <a href="https://navplot.asselect.uk/tomorrow_south.pdf" download={""}>
+              {"Download Tomorrow (England/Wales) PDF"}
+            </a>
+          </p>
+          <p class="mb-4">
+            <a href="https://navplot.asselect.uk/tomorrow_north.pdf" download={""}>
+              {"Download Tomorrow (North England/Scotland) PDF"}
+            </a>
+          </p>
         </div>
     }
 }
